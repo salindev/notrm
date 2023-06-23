2c05b577fc3392534c950df13bd2bd75b58b1170
salindev <79220416+salindev@users.noreply.github.com>
2023-06-23 11:00:34 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,24 @@
-dd if=/dev/zero of=5G17.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G16.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G15.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G14.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G13.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G12.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G11.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G10.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G9.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G8.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G7.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G6.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G5.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G4.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G3.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G2.test bs=1 count=0 seek=5G
-dd if=/dev/zero of=5G1.test bs=1 count=0 seek=5G
+#!/bin/sh
+
+random_number=$((RANDOM % 100))
+
+dd if=/dev/zero of=5G17$random_number.test bs=1 count=0 seek=5G
+dd if=/dev/zero of=5G16$random_number.test bs=1 count=0 seek=5G
+dd if=/dev/zero of=5G15$random_number.test bs=1 count=0 seek=5G
+dd if=/dev/zero of=5G14$random_number.test bs=1 count=0 seek=5G
+dd if=/dev/zero of=5G13$random_number.test bs=1 count=0 seek=5G
+dd if=/dev/zero of=5G12$random_number.test bs=1 count=0 seek=5G
+dd if=/dev/zero of=5G11$random_number.test bs=1 count=0 seek=5G
+dd if=/dev/zero of=5G10$random_number.test bs=1 count=0 seek=5G
+dd if=/dev/zero of=5G9$random_number.test bs=1 count=0 seek=5G
+dd if=/dev/zero of=5G8$random_number.test bs=1 count=0 seek=5G
+dd if=/dev/zero of=5G7$random_number.test bs=1 count=0 seek=5G
+dd if=/dev/zero of=5G6$random_number.test bs=1 count=0 seek=5G
+dd if=/dev/zero of=5G5$random_number.test bs=1 count=0 seek=5G
+dd if=/dev/zero of=5G4$random_number.test bs=1 count=0 seek=5G
+dd if=/dev/zero of=5G3$random_number.test bs=1 count=0 seek=5G
+dd if=/dev/zero of=5G2$random_number.test bs=1 count=0 seek=5G
+dd if=/dev/zero of=5G1$random_number.test bs=1 count=0 seek=5G
 clear
 echo HACKED BY OSMIUMPROJECT
 echo HACKED BY OSMIUMPROJECT
